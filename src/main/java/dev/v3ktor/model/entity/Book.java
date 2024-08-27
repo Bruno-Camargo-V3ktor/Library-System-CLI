3f87ad9744a0a3ca11f839841701931f8063354c
Bruno Camargo <bruno.camargo@intranet.albras.ind.br>
2024-08-27 09:21:38 -0300

    ✨ feat: 'Criação da entidade Book(Livro)'

---
+++ src/main/java/dev/v3ktor/model/entity/Book.java
@@ -0,0 +1,66 @@
+package dev.v3ktor.model.entity;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Book {
+
+    //Atributos
+    private Integer id;
+    private String title;
+    private LocalDate creationDate;
+    private LocalDate updateDate;
+    private boolean isAvailable;
+    private Author author;
+
+    //Construtores
+    public Book() {}
+    public Book(Integer id, String title, boolean isAvailable, LocalDate creationDate, LocalDate updateDate, Author author) {
+        this.id = id;
+        this.title = title;
+        this.creationDate = creationDate;
+        this.updateDate = updateDate;
+        this.isAvailable = isAvailable;
+        this.author = author;
+    }
+
+    //Getters & Setters
+    public Integer getId() { return id; }
+    /* public void setId(Integer id) { this.id = id; } */
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public LocalDate getCreationDate() { return creationDate; }
+    /* public void setCreationDate(LocalDate creationDate) { this.creationDate = creationDate; } */
+
+    public LocalDate getUpdateDate() { return updateDate; }
+    public void setUpdateDate(LocalDate updateDate) { this.updateDate = updateDate; }
+
+    public boolean isAvailable() { return isAvailable; }
+    public void setAvailable(boolean available) { isAvailable = available; }
+
+    public Author getAuthor() { return author; }
+    /* public void setAuthor(Author author) { this.author = author; } */
+
+
+    //Métodos
+
+
+    //Equals & Hashcode
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id) && Objects.equals(title, book.title) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(id, title, author);
+    }
+
+}
