3668da7bba70052abcf013edd6d6be160e39e110
Bruno Camargo <bruno.camargo@intranet.albras.ind.br>
2024-08-27 11:52:05 -0300

    ✨ feat: 'Criação da entidade BookLoan(Emprestimo de Livro)'

---
+++ src/main/java/dev/v3ktor/model/entity/BookLoan.java
@@ -0,0 +1,48 @@
+package dev.v3ktor.model.entity;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class BookLoan {
+
+    //Atributos
+    private Integer id;
+    private Book book;
+    private User user;
+    private LocalDate date;
+
+    //Construtores
+    public BookLoan() {}
+    public BookLoan(Book book, User user, LocalDate date) {
+        this.book = book;
+        this.user = user;
+        this.date = date;
+    }
+
+    //Getters & Setters
+    public Integer getId() { return id; }
+
+    public Book getBook() { return book; }
+
+    public User getUser() { return user; }
+
+    public LocalDate getDate() { return date; }
+
+
+    //Equals & Hashcode
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BookLoan bookLoan = (BookLoan) o;
+        return Objects.equals(id, bookLoan.id);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hashCode(id);
+    }
+
+}
