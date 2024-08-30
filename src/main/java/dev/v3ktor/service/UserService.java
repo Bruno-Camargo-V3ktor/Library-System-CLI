3b8acbfb7c0a6d7ac78b7de33f13b81b7f9e28a9
Bruno C. <v3ktoryt@gmail.com>
2024-08-30 14:07:20 -0300

    ✨ feat: 'Métodos de 'Pegar todos', 'Login', 'Atualizar usuario' foram implementados no Serviço de Usuario'

--- src/main/java/dev/v3ktor/service/UserService.java
+++ src/main/java/dev/v3ktor/service/UserService.java
@@ -1,6 +1,10 @@
 package dev.v3ktor.service;
 
+import dev.v3ktor.model.entity.User;
+import dev.v3ktor.model.enums.UserHoles;
 import dev.v3ktor.model.repository.UserRepository;
+import java.util.List;
+import java.util.Objects;
 
 public class UserService {
 
@@ -12,5 +16,25 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
+    // MÉTODOS
+    public List<User> getAllUsers( User actualUser )
+    {
+        if( !actualUser.getHoles().contains( UserHoles.ADMIN ) ) return null;
+        return userRepository.findAll();
+    }
+
+    public User login( String username, String password )
+    {
+        var user = userRepository.findByUsername( username );
+        if( !user.getPassword().equals( String.valueOf( password.hashCode() ) ) ) return null;
+
+        return user;
+    }
+
+    public void updateUser( User actualUser, User newUser )
+    {
+        if( Objects.equals(actualUser.getId(), newUser.getId()) ) return;
+        userRepository.update( newUser );
+    }
 
 }
