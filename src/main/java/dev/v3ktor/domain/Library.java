de1a78ef3075e4a07506e38cbe4c9378ebd85283
Bruno C. <v3ktoryt@gmail.com>
2024-08-31 23:22:51 -0300

    📦 refactor: 'Deixando mais semantico e adicionando mais da logica de negocio a classe de dominio Libary'

--- src/main/java/dev/v3ktor/domain/Library.java
+++ src/main/java/dev/v3ktor/domain/Library.java
@@ -3,6 +3,7 @@ package dev.v3ktor.domain;
 import dev.v3ktor.model.entity.Author;
 import dev.v3ktor.model.entity.Book;
 import dev.v3ktor.model.entity.BookLoan;
+import dev.v3ktor.model.entity.User;
 import dev.v3ktor.model.repository.AuthorRepository;
 import dev.v3ktor.model.repository.BookLoanRepository;
 import dev.v3ktor.model.repository.BookRepository;
@@ -22,6 +23,8 @@ public class Library {
     private final AuthorService authorService;
     private final LoanService loanService;
 
+    private User user;
+
     //Construtores
     public Library(BookRepository bookRepository, UserRepository userRepository, AuthorRepository authorRepository, BookLoanRepository bookLoanRepository) {
         this.bookService = new BookService( bookRepository );
@@ -29,5 +32,15 @@ public class Library {
         this.authorService = new AuthorService( authorRepository );
         this.loanService = new LoanService( bookLoanRepository );
     }
+
+    //GETTERS & SETTERS
+    public User getUser() { return user; }
+    public void setUser(User user) { this.user = user; }
+
+    public AuthorService authorService() { return authorService; }
+    public BookService bookService() { return bookService; }
+    public LoanService loanService() { return loanService; }
+    public UserService userService() { return userService; }
+
 
 }
