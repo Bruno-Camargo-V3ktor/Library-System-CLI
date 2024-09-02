f93c283e9f77448fce69049bba0d33a32f081a21
Bruno C. <v3ktoryt@gmail.com>
2024-09-02 14:19:12 -0300

    ✨ feat: 'Tela do Menu Principal implementada, faltando apenas fazer os devidos direcionamentos as outras telas'

--- src/main/java/dev/v3ktor/view/screens/HomeScreen.java
+++ src/main/java/dev/v3ktor/view/screens/HomeScreen.java
@@ -1,5 +1,7 @@
 package dev.v3ktor.view.screens;
 
+import dev.v3ktor.domain.Library;
+import dev.v3ktor.model.enums.UserHoles;
 import dev.v3ktor.util.Props;
 import dev.v3ktor.util.ViewUtils;
 import dev.v3ktor.view.View;
@@ -20,21 +22,69 @@ public class HomeScreen extends View {
     //MÉTODOS
     @Override
     public void render() {
+        var libary = props.get( "libary", Library.class );
+        var user = libary.getUser();
+
         System.out.println( ViewUtils.head );
 
         System.out.println("╔════════════════════════════════════════╗");
         System.out.println("║             MENU PRINCIPAL             ║");
+
+        System.out.println( !user.getHoles().contains( UserHoles.ADMIN )
+                        ?  "║                (CLIENT)                ║"
+                        :  "║                (ADMIN)                 ║"
+                        );
+
         System.out.println("║           -----------------            ║");
         System.out.println("║                                        ║");
-        //System.out.println("║ [1] Login    [2] Cadastra    [3] Sair  ║");
+        System.out.println("║ [1] Livros [2] Emprestimos [3] Autores ║");
+        System.out.println("║       [4] Cadastros  [5] Logout        ║");
         System.out.println("╚════════════════════════════════════════╝");
 
-        sc.nextLine();
+
+        Integer op = null;
+        while(op == null)
+        {
+            System.out.print( "\n ║--> " );
+            try
+            {
+                op = Integer.parseInt( sc.nextLine() );
+                if( op < 1 || op > 5 ) throw new RuntimeException();
+            }
+            catch (RuntimeException e)
+            {
+                op = null;
+                System.out.print("Opção Invalida... tente novamente\n");
+            }
+
+        }
+
+        switch( op )
+        {
+            case 1:
+                break;
+
+            case 2:
+                break;
+
+            case 3:
+                break;
+
+            case 4:
+                break;
+
+            case 5:
+                libary.setUser(null);
+                manager.to("login", props);
+                break;
+        }
+
     }
 
     @Override
     public void close() {
         ViewUtils.clearScreen();
     }
+
 
 }
